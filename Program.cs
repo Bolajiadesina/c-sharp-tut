6a81d3607196f4a3344a2592409e1bdfd6c03195
Bolaji Adesina <adesina.bolaji@yahoo.com>
2024-05-25 10:37:13 +0100

    adding initial commit for c# tutorial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var number= 2;
+        var count = 10;
+        var totalPrice = 20.95f;
+        var character = 'A';
+        var firstName = "Bolaji";
+        var isWorking = false;
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(number);
+        Console.WriteLine(count);
+        Console.WriteLine(totalPrice);
+        Console.WriteLine(character);
+        Console.WriteLine(firstName);
+        Console.WriteLine(isWorking);
+    }
+}
